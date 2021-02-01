b3b8dc0d6d92451fd578065419cc000421e003cd
imd <pradoukraine@gmail.com>
2021-02-01 02:50:21 +0200

    Draw badge background

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Mac on 01.02.2021.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
